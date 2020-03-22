b6077aabc516d72fb9196b44489ad4b085e8b833
ranjanistic <priyanshuranjan88@gmail.com>
2020-03-23 00:36:49 +0530

    wifi + screen +all apps launch

--- app/src/main/java/org/ranjanistic/skivvy/Administrator.kt
+++ app/src/main/java/org/ranjanistic/skivvy/Administrator.kt
@@ -3,48 +3,15 @@ package org.ranjanistic.skivvy
 import android.app.admin.DeviceAdminReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.widget.Toast
 
-
 class Administrator : DeviceAdminReceiver() {
-    private fun showToast(context: Context?, msg: CharSequence?) {
-        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+    override fun onEnabled(context: Context, intent: Intent) {
+        super.onEnabled(context, intent)
+        Toast.makeText(context, "Enabled", Toast.LENGTH_SHORT).show()
     }
-
-    override fun onEnabled(context: Context?, intent: Intent?) {
-        showToast(context, "Sample Device Admin: enabled")
-    }
-
-    override fun onDisableRequested(context: Context?, intent: Intent?): CharSequence {
-        return "This is an optional message to warn the user about disabling."
-    }
-
-    override fun onDisabled(context: Context?, intent: Intent?) {
-        showToast(context, "Sample Device Admin: disabled")
-    }
-
-    override fun onPasswordChanged(context: Context?, intent: Intent?) {
-        showToast(context, "Sample Device Admin: pw changed")
-    }
-
-    override fun onPasswordFailed(context: Context?, intent: Intent?) {
-        showToast(context, "Sample Device Admin: pw failed")
-    }
-
-    override fun onPasswordSucceeded(context: Context?, intent: Intent?) {
-        showToast(context, "Sample Device Admin: pw succeeded")
-    }
-
-    companion object {
-        fun getSamplePreferences(context: Context): SharedPreferences {
-            return context.getSharedPreferences(
-                DeviceAdminReceiver::class.java.name, 0
-            )
-        }
-
-        var PREF_PASSWORD_QUALITY = "password_quality"
-        var PREF_PASSWORD_LENGTH = "password_length"
-        var PREF_MAX_FAILED_PW = "max_failed_pw"
+    override fun onDisabled(context: Context, intent: Intent) {
+        super.onDisabled(context, intent)
+        Toast.makeText(context, "Disabled", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/org/ranjanistic/skivvy/MainActivity.kt
+++ app/src/main/java/org/ranjanistic/skivvy/MainActivity.kt
@@ -7,9 +7,14 @@ import android.bluetooth.BluetoothAdapter
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.location.LocationManager
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.os.Environment
+import android.provider.Settings
 import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
 import android.text.format.DateFormat
@@ -21,7 +26,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
-import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import org.ranjanistic.skivvy.R.drawable.*
 import org.ranjanistic.skivvy.R.string.*
@@ -31,8 +35,6 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    var deviceManger: DevicePolicyManager? = null
-    var compName: ComponentName? = null
     var activityManager: ActivityManager? = null
     private var tts: TextToSpeech? = null
     private var outPut: TextView? = null
@@ -41,24 +43,31 @@ class MainActivity : AppCompatActivity() {
     private var fadeAnimation: Animation? = null
     private var receiver: TextView? = null
     private var CODE_SPEECH_RECORD = 10
+    private var CODE_OTHER_APP = 11
+    private var CODE_LOCATION_SERVICE = 100
+    private var CODE_LOCK_SCREEN = 101
     private var loading: ImageView? = null
     private var icon: ImageView? = null
     private var txt: String? = null
+    private lateinit var packagesAppName:Array<String?>
+    private lateinit var packagesName:Array<String?>
+    private lateinit var packagesMain:Array<Intent?>
+    private var deviceManger: DevicePolicyManager? = null
+    private var compName: ComponentName? = null
+    var adminActive:Boolean? = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        deviceManger = getSystemService(
-            Context.DEVICE_POLICY_SERVICE
-        ) as DevicePolicyManager
-        activityManager = getSystemService(
-            Context.ACTIVITY_SERVICE
-        ) as ActivityManager
+        deviceManger = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         compName = ComponentName(this, Administrator::class.java)
+        adminActive = deviceManger!!.isAdminActive(compName!!)
 
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.setting)
             .setOnClickListener {
                 setTheme(R.style.DarkTheme)
             }
+        getPackages()
         outPut = findViewById(R.id.textOutput)
         loading = findViewById(R.id.loader)
         loading?.setImageDrawable(getDrawable(ic_dotsincircle))
@@ -80,13 +89,18 @@ class MainActivity : AppCompatActivity() {
         })
 
         receiver?.setOnClickListener {
-            loading?.setImageDrawable(getDrawable(ic_dotsincircle))
-            loading?.visibility = View.VISIBLE
-            loading?.startAnimation(rotateAnimation)
-            outPut?.text = null
-            icon?.setImageDrawable(null)
+            normalView()
             startVoiceRecIntent()
         }
+    }
+
+    private fun normalView(){
+        txt = null
+        loading?.setImageDrawable(getDrawable(ic_dotsincircle))
+        loading?.visibility = View.VISIBLE
+        loading?.startAnimation(rotateAnimation)
+        outPut?.text = null
+        icon?.setImageDrawable(null)
     }
 
     private fun startVoiceRecIntent(){
@@ -106,12 +120,18 @@ class MainActivity : AppCompatActivity() {
         tts!!.language = Locale.US
         when(requestCode) {
             CODE_SPEECH_RECORD -> {
+                @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
                 if (resultCode == Activity.RESULT_OK && data != null)
-                    txt = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)[0].toLowerCase(Locale.ROOT)
+                    txt = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)[0].toString().toLowerCase(Locale.US)
                 if(txt!=null) {
                     outPut?.text = txt
-                    respondToCommand(txt)
-                    txt = null
+                    if(!respondToCommand(txt)) {
+                        if (!appOptions(txt)) {
+                            errorView()
+                            outPut?.text = getString(recognize_error)
+                            speakOut(getString(recognize_error))
+                        }
+                    }
                 }
                 else {
                     errorView()
@@ -119,88 +139,139 @@ class MainActivity : AppCompatActivity() {
                     speakOut(getString(no_input))
                 }
             }
-        } 
+            CODE_OTHER_APP ->{
+                speakOut("Job done")
+            }
+            CODE_LOCATION_SERVICE ->{
+                val locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    successView()
+                    speakOut("All satellites on you now")
+                } else {
+                    errorView()
+                    speakOut("GPS not enabled")
+                }
+            }
+            CODE_LOCK_SCREEN ->{
+                if(resultCode == Activity.RESULT_OK){
+                    deviceLockOps()
+                } else {
+                    errorView()
+                    outPut?.text = getString(device_admin_request)
+                    speakOut(getString(device_admin_request))
+                }
+            }
+        }
     }
 
-    private fun respondToCommand(txt:String?){
-        var localTxt = txt
-        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    private fun respondToCommand(text:String?):Boolean{
         var i = 0
         var flag = false
-        val arr: IntArray = intArrayOf(bt, screenshot, lock_screen, wifi, gps)
+        val arr: IntArray = intArrayOf(bt, screenshot, lock_screen, wifi, gps, wi_fi)
         while (i < arr.size) {
-            if (localTxt == (getString(arr[i]))) {
+            if (text == (getString(arr[i]))) {
+                flag = true
                 outPut?.text = getString(arr[i])
-                successView()
-                if (localTxt == getString(bt)) {
-                    icon!!.setImageDrawable(getDrawable(ic_bluetooth))
-                    if (mBluetoothAdapter.isEnabled) {
-                        errorView()
-                        speakOut("Bluetooth is already on")
-                    } else {
-                        speakOut("Turning on bluetooth")
-                        mBluetoothAdapter.enable()
-                    }
-                } else if (localTxt == getString(wifi)) {
-                    speakOut("Turning on Wi-Fi")
-                } else if (localTxt == getString(gps)) {
-                    speakOut("All satelllites on you")
-                } else if (localTxt == getString(lock_screen)) {
-                    val active: Boolean? =
-                        compName?.let { deviceManger?.isAdminActive(it) }
-                    if (active!!) {
-                        deviceManger?.lockNow()
-                        speakOut("Locked")
-                    }
-                } else if (localTxt == getString(screenshot)) {
-                    speakOut("screen shot taken")
+                if (text == getString(bt)) {
+                    bluetoothOps()
+                } else if (text == getString(wifi)) {
+                    wifiOps()
+                } else if(text == getString(wi_fi)){
+                    wifiOps()
+                } else if (text == getString(gps)) {
+                    locationOps()
+                } else if (text == getString(lock_screen)) {
+
+                    deviceLockOps()
+                } else if (text == getString(screenshot)) {
                     takeScreenshot()
                 } else {
-                    speakOut("Will understand it soon")
                     flag = false
                     break
                 }
-                flag = true
-                localTxt = null
                 break
             }
             ++i
         }
-        if (!flag) {
-            when (localTxt) {
-                "whatsapp" -> {
-                    successView()
-                    val intent = Intent(Intent.ACTION_MAIN)
-                    intent.component =
-                        ComponentName("com.whatsapp", "com.whatsapp.Main")
-                    startActivity(intent)
-                    localTxt = null
+        return flag
+    }
+    private fun bluetoothOps(){
+        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        icon!!.setImageDrawable(getDrawable(ic_bluetooth))
+        if (mBluetoothAdapter.isEnabled) {
+            errorView()
+            mBluetoothAdapter.disable()
+            speakOut("Bluetooth is off")
+        } else {
+            successView()
+            mBluetoothAdapter.enable()
+            speakOut("Bluetooth is on")
+        }
+    }
+    private fun wifiOps(){
+        val wifiManager: WifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
+        if(wifiManager.isWifiEnabled) {
+            errorView()
+            wifiManager.isWifiEnabled = false
+            icon!!.setImageDrawable(getDrawable(ic_wifi_disconnected))
+            speakOut("Turned Wi-Fi off")
+        } else {
+            successView()
+            wifiManager.isWifiEnabled = true
+            icon!!.setImageDrawable(getDrawable(ic_wifi_connected))
+            speakOut("Wi-Fi is looking for available hot spots")
+        }
+    }
+    private fun locationOps(){
+        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivityForResult(intent,CODE_LOCATION_SERVICE);
+    }
+    private fun deviceLockOps(){
+        deviceManger = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        icon!!.setImageDrawable(getDrawable(ic_glossylock))
+        if (deviceManger!!.isAdminActive(ComponentName(this, Administrator::class.java))) {
+            successView()
+            speakOut("Locking")
+            deviceManger!!.lockNow()
+        } else {
+            speakOut(getString(device_admin_request))
+            startActivityForResult(Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
+                .putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName)
+                .putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, getString(device_admin_persuation)), CODE_LOCK_SCREEN)
+        }
+    }
+
+    private fun appOptions(text: String?):Boolean{
+        var flag = false
+        var i = 0
+        if(text!=null) {
+            while (i < packagesName.size) {
+                if (text.let { packagesAppName[i]!!.indexOf(it) } != -1) {
+                    flag = successView()
+                    startActivityForResult(Intent(packagesMain[i]), CODE_OTHER_APP)
+                    break
+                } else if (text.let { packagesName[i]!!.indexOf(it) } != -1) {
+                    flag = successView()
+                    startActivityForResult(Intent(packagesMain[i]), CODE_OTHER_APP)
+                    break
                 }
-                "insta", "instagram" -> {
-                    successView()
-                    val intent = Intent(Intent.ACTION_MAIN)
-                    intent.component = ComponentName(
-                        "com.instagram.android",
-                        "com.instagram.mainactivity.LauncherActivity"
-                    )
-                    startActivity(intent)
-                    localTxt = null
-                }
-                "camera" -> {
-                    successView()
-                    val intent = Intent(Intent.ACTION_MAIN)
-                    intent.component =
-                        ComponentName("com.android.camera", "com.android.camera.Camera")
-                    startActivity(intent)
-                    localTxt = null
-                }
-                else -> {
-                    errorView()
-                    outPut?.text = getString(recognize_error)
-                    speakOut(getString(recognize_error))
-                    localTxt = null
-                }
+                ++i
             }
+        } else {
+            flag = errorView()
+        }
+        return flag
+    }
+    private fun getPackages(){
+        val pm: PackageManager = packageManager
+        val packages: List<ApplicationInfo> = pm.getInstalledApplications(PackageManager.GET_META_DATA)
+        packagesAppName = arrayOfNulls(packages.size)
+        packagesName = arrayOfNulls(packages.size)
+        packagesMain = arrayOfNulls(packages.size)
+        for ((counter, packageInfo) in packages.withIndex()) {
+            packagesAppName[counter] = pm.getApplicationLabel(packageInfo).toString()
+            packagesName[counter] = packageInfo.packageName
+            packagesMain[counter] = pm.getLaunchIntentForPackage(packageInfo.packageName)
         }
     }
     private fun takeScreenshot() {
@@ -214,10 +285,11 @@ class MainActivity : AppCompatActivity() {
             v1.isDrawingCacheEnabled = false
             val imageFile = File(mPath)
             val outputStream = FileOutputStream(imageFile)
-            val quality = 500
+            val quality = 100
             bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream)
             outputStream.flush()
             outputStream.close()
+            speakOut("screen shot taken")
         } catch (e: Throwable) {
             errorView()
             speakOut("Can't take screenshot")
@@ -234,16 +306,19 @@ class MainActivity : AppCompatActivity() {
      }
 
     override fun onBackPressed() {
+        speakOut("Bye")
         loading?.startAnimation(exitAnimation)
         super.onBackPressed()
     }
 
-    private fun errorView(){
+    private fun errorView():Boolean{
         loading?.startAnimation(fadeAnimation)
         loading?.setImageDrawable(getDrawable(ic_red_dotsincircle))
+        return false
     }
-    private fun successView(){
+    private fun successView():Boolean{
         loading?.setImageDrawable(getDrawable(ic_green_dotsincircle))
+        return true
     }
      private fun speakOut(text:String) {
          tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null,"")
